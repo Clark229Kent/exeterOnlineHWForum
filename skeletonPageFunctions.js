1e9aec5fae47eb2d83126c1883e5f15a982e590d
Clark S. Wu <74661468+Clark229Kent@users.noreply.github.com>
2023-05-09 13:18:40 -0400

    Add files via upload

--- skeletonPageFunctions.js
+++ skeletonPageFunctions.js
@@ -40,6 +40,83 @@ function submitPost(){
   window.location.href='skeletonPostPage.html';
 }
 
+function createTable()
+{
+  /*right now it prompts the user for number of rows and columns but eventually
+  that number will be determined by the number of comments*/
+rn = window.prompt("Input number of rows", 1);
+cn = window.prompt("Input number of columns",1);
+  
+ for(var r=0;r<parseInt(rn,10);r++)
+  {
+   var x=document.getElementById("myTable").insertRow(r);
+   for(var c=0;c<parseInt(cn,10);c++)  
+    {
+     var y=  x.insertCell(c);
+     y.innerHTML="Row-"+r+" Column-"+c; 
+    }
+   }
+}
+
+function createReplyForm(){
+ for(let r=0;r<2;r++)
+  {
+   let x=document.getElementById("replyFormTable").insertRow(r);
+   for(let c=0;c<2;c++)  
+    {
+      if (r == 0){
+        let y=  x.insertCell(c);
+        y.id = "contentRow";
+        document.getElementById("contentRow").colSpan = "2";
+        document.getElementById("contentRow").innerHTML =
+         '<textarea placeholder="What you are curious about:" id="replyContentText" name="replyContentText"></textarea><br><br>';
+        break;
+      }
+      else{
+        let y=  x.insertCell(c);
+        y.id = "NOTcontentRow";
+        document.getElementById("NOTcontentRow").colSpan = "1";
+        if (r == 1 && c == 0){
+          y.innerHTML = '<button class = "submittable" id = "replySubmitButton" onclick = "createSingleComment()" > Submit </button>';
+        }
+      }
+    //y.innerHTML="Row-"+r+" Column-"+c; 
+    }
+   }
+}
+
+
+
+function createSingleComment()
+{
+ for(let r=0;r<3;r++)
+  {
+   let x=document.getElementById("postedRepliesTable").insertRow(r);
+   for(let c=0;c<4;c++)  
+    {
+      if (r == 1){
+        let y=  x.insertCell(c);
+        y.id = "contentRow";
+        document.getElementById("contentRow").colSpan = "4";
+        break;
+      }
+      else{
+        let y =  x.insertCell(c);
+        y.id = "NOTcontentRow";
+        document.getElementById("NOTcontentRow").colSpan = "1";
+        if (r == 0 && c == 0){
+          y.innerHTML = "Resolved/Unresolved";
+        }
+        if (r == 2 && c == 0){
+          y.innerHTML = "upvote";
+        }
+      }
+     //y.innerHTML="Row-"+r+" Column-"+c; 
+    }
+   }
+}
+
+/*
 function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
@@ -57,4 +134,4 @@ var mountains = [
   { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
   { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
   { name: "Monte Amiata", height: 1738, place: "Siena" }
-];
+];*/
