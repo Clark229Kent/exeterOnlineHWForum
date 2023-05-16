a37866f48af32e55bf0ac5beed8949100daebdf2
Clark S. Wu <74661468+Clark229Kent@users.noreply.github.com>
2023-05-16 12:08:15 -0400

    Add files via upload

--- addPost.php
+++ addPost.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = '127.0.0.1:3306';
-    $username = 'root';
-    $password = '';
-    $dbname = 'test';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "may16tester";
     $conn = new mysqli($servername, $username, $password, $dbname);
     
     if($conn->connect_error){
