4754f6701902a53335f64aa036bffe2615971c7b
lsbrown4287 <62302874+lsbrown4287@users.noreply.github.com>
2023-05-24 12:49:24 -0400

    Create addComment.php

---
+++ addComment.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = '127.0.0.1:3306';
+    $username = 'root';
+    $password = '';
+    $dbname = 'poststorageschema';
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    if($conn->connect_error){
+        die('Connection failed');
+    }
+    $id = $_GET['id'];
+    $date = date("Y-m-d H:i:s");
+    $content = $_POST['replyContentText'];
+    $sql = "INSERT INTO poststoragetable (logtime, content, score, parent)
+            VALUES ('$date', '$content', 0, '$id')";
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>
+            alert('works!');
+            </script>";
+    }
+    else{
+        echo $conn->error;
+    }
+    header("Location: http://localhost/exeterOnlineHWForum-main/exeterOnlineHWForum-main/postPage.php?id=" . $id);
+    $conn->close();
+?>
