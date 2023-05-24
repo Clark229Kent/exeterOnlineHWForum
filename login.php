3ac2d9f1d845ee6255ccc5aae8fabce6313154c0
Clark S. Wu <74661468+Clark229Kent@users.noreply.github.com>
2023-05-24 15:03:43 -0400

    Update login.php

--- login.php
+++ login.php
@@ -20,20 +20,25 @@
     $row = mysqli_fetch_assoc($result);
 
     if ($row) {
+        $user_type = $row["user_type"];
+
         if ($row["user_type"] == "reg") {
             session_start();
             $_SESSION["username"] = $username;
-            $response = array('userType' => 'regular');
-            echo json_encode($response);
+            $_SESSION["user_type"] = 'regular';
+
         } else if ($row["user_type"] == "mod") {
             session_start();
             $_SESSION["username"] = $username;
             $_SESSION["user_type"] = "moderator";
-            $response = array('userType' => 'moderator');
-            echo json_encode($response);
+
         }
+        header("Location: skeletonHomePageForum.php");
+        exit();
     } else {
         $errorMessage = "Invalid username or password";
+        echo json_encode(array("errorMessage" => $errorMessage));
+        exit();
     }
     
 
